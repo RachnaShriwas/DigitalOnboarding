50c77eaf70ec4618378d1ad61bb8fda0931598d4
Rachna Shriwas <rachnashriwas@gmail.com>
2019-11-10 09:39:31 +0530

    v1 commit

--- app/src/main/java/com/example/myapplication34/SuccessPage.java
+++ app/src/main/java/com/example/myapplication34/SuccessPage.java
@@ -1,7 +1,24 @@
 package com.example.myapplication34;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SuccessPage extends AppCompatActivity {
+import butterknife.BindView;
 
+public class SuccessPage extends AppCompatActivity {
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        Log.d("TAG", "*****in MAIN model loaded******");
+
+        setContentView(R.layout.success);
+    }
 }
